881b1ec51fbfff333526db4e53546774fa8ae5cc
Roger Baker <rogpog13@gmail.com>
2016-04-19 22:21:20 -0400

    Resetting source.

---
+++ example.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from bootstrapping import *
+from middleware import *
+#peerIP, peerTimes, peerRecordID = getPeerIP()
+
+#print (peerIP)
+#print (peerTimes)
+#print (peerRecordID)
+
+#To Remove Oldest IP Address
+#removeOldestIPEntry()
+#postPass2DNS("7978...a8d1:password_cracked")
+#To Add current Host
+#postHostIP()
+
+
+
+
+crack("NTLM", 4, "l", "C78031B90B8EE8E3D72678D524A889D0", 250000, 350000)
